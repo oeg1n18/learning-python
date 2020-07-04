0d6fc467e353f5c5be40a365befd5b5a51fe83bc
oeg1n18 <oeg1n18@soton.ac.uk>
2020-07-04 15:51:02 +0100

    initial commit of Numeric types

--- numeric_types.py
+++ numeric_types.py
@@ -1,1 +1,10 @@
-hello world
+#========================== Numeric literals ==================================
+1234 #integers
+1.23 #Floating-point Numbers
+0x9ff #hex
+3+4j #complex
+set('spam'),{1,2,3,4} #construction forms
+x=1
+q = bool(x)    #Boolean
+y = True
+z = False
